9777b7c3a7d1f2a4ca9d023f7b9e6c51d541e99f
Khalil Stemmler <metroidman12@gmail.com>
2023-06-09 17:40:01 -0400

    Red: e2e test for checking the health of the server.

--- src/e2e.ispec.ts
+++ src/e2e.ispec.ts
@@ -1,8 +1,29 @@
 
+import request from 'supertest'
+import { Server } from './server';
+
 describe('e2e', () => {
   describe('health check', () => {
-    it('can check in on the health of the server', () => {
-      expect(true).toBeTruthy();
+
+    let server: Server;
+
+    beforeAll(async () => {
+      await server.start();
+    })
+
+    afterAll(async() => {
+      await server.stop();
+    })
+
+    it('can check in on the health of the server', async () => {
+      let response = await request(server.getExpressInstance())
+        .get('/health')
+        .set('Accept', 'application/json');
+
+      expect(response.headers["Content-Type"]).toMatch(/json/);
+      expect(response.status).toEqual(200);
+      expect(response.body.email).toEqual('foo@bar.com');
+
     })
   })
 })
---
+++ src/server.ts
@@ -0,0 +1,25 @@
+
+export class Server {
+
+  start () {
+
+  }
+
+  stop () {
+
+  }
+
+  getExpressInstance () {
+    
+  }
+
+}
+
+// const request = require('supertest');
+//       const express = require('express');
+
+//       const app = express();
+
+//       app.get('/user', function(req, res) {
+//         res.status(200).json({ name: 'john' });
+//       });
